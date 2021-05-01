b67fba2e268cd7bb0a8ce6aa1075bb00ea945f98
walkgs <walkingfgxxt@gmail.com>
2021-05-01 10:25:51 -0300

    Updated

---
+++ cojut/src/main/java/com/github/walkgs/cojt/cojut/Scope.java
@@ -0,0 +1,14 @@
+package com.github.walkgs.cojt.cojut;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Documented
+@Retention(RUNTIME)
+@Target(ANNOTATION_TYPE)
+public @interface Scope {
+}
